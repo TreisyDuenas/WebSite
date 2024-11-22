2fe7a1245ad6e1ded8e73b5aa95203da747b08d0
DargivV <golden.clave@gmail.com>
2024-11-22 12:47:48 -0500

    Primer Modulo de bton 7 se me fue el tiempo

---
+++ tareaUNoclase.js
@@ -0,0 +1,40 @@
+var nom = "Daniel RV";
+let edad = 19;
+const pi = 3.14;
+
+console.log(nombre);
+
+console.log(edad);
+
+console.log(pi);
+
+
+function saludar(){
+
+return 'hola ,' +{nombre}+'tienes' +{edad} +'años.';
+}
+
+function denegar(){
+
+    return 'no tienes la edad para esta vaina ';
+    }
+
+    
+if (edad>=18)
+console.log(saludar());
+else {console.log(denegar());} 
+
+
+let numero = 0;  
+for (let i = 0; i < 5; i++) {
+    numero = numero + 2;
+    console.log(numero);
+}
+
+let numero2 = 0;  
+for (let i = 1; i < 7; i++) {
+    numero2 = numero2 + 6;
+    console.log('6* ' + i + '= '+ numero2);
+}
+
+
